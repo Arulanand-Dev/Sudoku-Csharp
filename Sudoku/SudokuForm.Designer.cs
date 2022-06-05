9d9f25fe6a25fd4a78ae32a73b26135f9bb30dc2
Arulanand-Dev <arulanand157@outlook.com>
2022-06-05 17:47:22 +0530

    Add project files.

---
+++ Sudoku/SudokuForm.Designer.cs
@@ -0,0 +1,279 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    partial class SudokuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle = new DataGridViewCellStyle();
+            this.btnSolution = new System.Windows.Forms.Button();
+            this.ComboBox1 = new System.Windows.Forms.ComboBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnVerify = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSolution
+            // 
+            this.btnSolution.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSolution.Location = new System.Drawing.Point(543, 427);
+            this.btnSolution.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSolution.Name = "btnSolution";
+            this.btnSolution.Size = new System.Drawing.Size(160, 40);
+            this.btnSolution.TabIndex = 9;
+            this.btnSolution.Text = "Solution";
+            this.btnSolution.UseVisualStyleBackColor = true;
+            // 
+            // ComboBox1
+            // 
+            this.ComboBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComboBox1.FormattingEnabled = true;
+            this.ComboBox1.Items.AddRange(new object[] {
+            "Most",
+            "Medium",
+            "Least"});
+            this.ComboBox1.Location = new System.Drawing.Point(90, 10);
+            this.ComboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.ComboBox1.Name = "ComboBox1";
+            this.ComboBox1.Size = new System.Drawing.Size(234, 32);
+            this.ComboBox1.TabIndex = 8;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.Location = new System.Drawing.Point(12, 14);
+            this.Label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(55, 24);
+            this.Label1.TabIndex = 7;
+            this.Label1.Text = "Clues";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.Location = new System.Drawing.Point(543, 89);
+            this.btnNew.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(160, 40);
+            this.btnNew.TabIndex = 6;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.AllowUserToAddRows = false;
+            this.DataGridView1.AllowUserToDeleteRows = false;
+            this.DataGridView1.AllowUserToOrderColumns = true;
+            this.DataGridView1.AllowUserToResizeColumns = false;
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.ColumnHeadersVisible = false;
+            this.DataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            dataGridViewCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle.Font = new System.Drawing.Font("Calibri", 12F);
+            dataGridViewCellStyle.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DataGridView1.DefaultCellStyle = dataGridViewCellStyle;
+
+            this.DataGridView1.Location = new System.Drawing.Point(16, 71);
+            this.DataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.RowHeadersVisible = false;
+            this.DataGridView1.RowHeadersWidth = 51;
+            this.DataGridView1.RowTemplate.Height = 40;
+            this.DataGridView1.Size = new System.Drawing.Size(485, 450);
+            this.DataGridView1.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 40;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 40;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 40;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 40;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 40;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 40;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 40;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Column8";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 40;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Column9";
+            this.Column9.MinimumWidth = 6;
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 40;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(543, 166);
+            this.btnCheck.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(160, 40);
+            this.btnCheck.TabIndex = 10;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(543, 250);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(160, 40);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerify.Location = new System.Drawing.Point(543, 336);
+            this.btnVerify.Margin = new System.Windows.Forms.Padding(4);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(160, 40);
+            this.btnVerify.TabIndex = 12;
+            this.btnVerify.Text = "Verify";
+            this.btnVerify.UseVisualStyleBackColor = true;
+            this.btnVerify.Click += new System.EventHandler(this.btnVerify_Click);
+            // 
+            // SudokuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 556);
+            this.Controls.Add(this.btnVerify);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnSolution);
+            this.Controls.Add(this.ComboBox1);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.DataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "SudokuForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sudoku cs";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnSolution;
+        internal System.Windows.Forms.ComboBox ComboBox1;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Button btnNew;
+        internal System.Windows.Forms.DataGridView DataGridView1;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        internal System.Windows.Forms.Button btnCheck;
+        internal System.Windows.Forms.Button btnClear;
+        internal System.Windows.Forms.Button btnVerify;
+    }
+}
---
+++ Sudoku/SudokuForm.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class SudokuForm : Form
+    {
+        private Sudoku game = new Sudoku();
+        private Random random = new Random();
+
+        public SudokuForm()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+            btnNew.Click += btnNew_Click;
+            btnSolution.Click += btnSolution_Click;
+            DataGridView1.Paint += DataGridView1_Paint;
+            ComboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
+            game.ShowClues += game_ShowClues;
+            game.ShowSolution += game_ShowSolution;
+            game.ClearInputs += game_ClearInputs;
+            game.VerifyInputs += game_VerifyInputs;
+        }
+
+        private void Form1_Load(System.Object sender, System.EventArgs e)
+        {
+            DataGridView1.Rows.Add(9);
+            ComboBox1.SelectedIndex = 0;
+            btnNew.PerformClick();
+        }
+
+        private void btnNew_Click(System.Object sender, System.EventArgs e)
+        {
+            game.NewGame(random);
+        }
+
+        private void DataGridView1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(new Pen(Color.Black, 1), 120, 0, 120, 450);
+            e.Graphics.DrawLine(new Pen(Color.Black, 1), 240, 0, 240, 450);
+            e.Graphics.DrawLine(new Pen(Color.Black, 1), 0, 122, 485, 122);
+            e.Graphics.DrawLine(new Pen(Color.Black, 1), 0, 244, 485, 244);
+        }
+
+        private void btnSolution_Click(System.Object sender, System.EventArgs e)
+        {
+            game.showGridSolution();
+        }
+
+        private void ComboBox1_SelectedIndexChanged(System.Object sender, System.EventArgs e)
+        {
+            btnNew.PerformClick();
+        }
+
+        public void game_ShowClues(int[][] grid)
+        {
+            for (int y = 0; y <= 8; y++)
+            {
+                List<int> cells = new List<int>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+                for (int c = 1; c <= 9 - (5 - ComboBox1.SelectedIndex); c++)
+                {
+                    int randomNumber = cells[random.Next(0, cells.Count())];
+                    cells.Remove(randomNumber);
+                }
+                for (int x = 0; x <= 8; x++)
+                {
+                    if (cells.Contains(x + 1))
+                    {
+                        DataGridView1.Rows[y].Cells[x].Value = grid[y][x];
+                        DataGridView1.Rows[y].Cells[x].Style.ForeColor = Color.Black;
+                        DataGridView1.Rows[y].Cells[x].ReadOnly = true;
+                    }
+                    else
+                    {
+                        DataGridView1.Rows[y].Cells[x].Value = "";
+                        DataGridView1.Rows[y].Cells[x].Style.ForeColor = Color.Blue;
+                        DataGridView1.Rows[y].Cells[x].ReadOnly = false;
+                    }
+                }
+            }
+        }
+
+        public void game_ShowSolution(int[][] grid)
+        {
+            for (int y = 0; y <= 8; y++)
+            {
+                for (int x = 0; x <= 8; x++)
+                {
+                    if (DataGridView1.Rows[y].Cells[x].Style.ForeColor != Color.Black)
+                    {
+                        if (string.IsNullOrEmpty(DataGridView1.Rows[y].Cells[x].Value.ToString()))
+                        {
+                            DataGridView1.Rows[y].Cells[x].Style.ForeColor = Color.Green;
+                            DataGridView1.Rows[y].Cells[x].Value = grid[y][x];
+                        }
+                        else
+                        {
+                            if (grid[y][x].ToString() != DataGridView1.Rows[y].Cells[x].Value.ToString())
+                            {
+                                DataGridView1.Rows[y].Cells[x].Style.ForeColor = Color.Green;
+                                DataGridView1.Rows[y].Cells[x].Value = grid[y][x];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            game.clearInputs();
+        }
+
+        public void game_ClearInputs(int[][] grid)
+        {
+            for (int y = 0; y <= 8; y++)
+            {
+                for (int x = 0; x <= 8; x++)
+                {
+                    if (DataGridView1.Rows[y].Cells[x].Style.ForeColor != Color.Black)
+                    {
+                        if (!string.IsNullOrEmpty(DataGridView1.Rows[y].Cells[x].Value.ToString()))
+                        {
+                            DataGridView1.Rows[y].Cells[x].Value = string.Empty;
+                            DataGridView1.Rows[y].Cells[x].Style.ForeColor = Color.Blue;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void game_VerifyInputs(int[][] grid)
+        {
+            for (int y = 0; y <= 8; y++)
+            {
+                for (int x = 0; x <= 8; x++)
+                {
+                    if (DataGridView1.Rows[y].Cells[x].Style.ForeColor != Color.Black)
+                    {
+                        if (grid[y][x].ToString() == DataGridView1.Rows[y].Cells[x].Value.ToString())
+                        {
+                            DataGridView1.Rows[y].Cells[x].Style.ForeColor = Color.Green;
+                        }
+                        else if (!string.IsNullOrEmpty(DataGridView1.Rows[y].Cells[x].Value.ToString()))
+                        {
+                            DataGridView1.Rows[y].Cells[x].Style.ForeColor = Color.Red;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnVerify_Click(object sender, EventArgs e)
+        {
+            game.verifyInputs();
+        }
+    }
+}
